cebf0b7f0a0af57e169b25234d61f94115722661
ProjectScanner <rahmandap214@gmail.com>
2024-02-16 08:58:32 +0700

    test

--- lib/dasboard.dart
+++ lib/dasboard.dart
@@ -1,34 +1,30 @@
 import 'package:flutter/material.dart';
 
-class dasboard extends StatefulWidget {
-  const dasboard({Key? key}) : super(key: key);
+class Dasboard extends StatefulWidget {
+  const Dasboard({Key? key}) : super(key: key);
 
   @override
-  State<dasboard> createState() => _dasboardState();
+  State<Dasboard> createState() => _DasboardState();
 }
 
-class _dasboardState extends State<dasboard> {
+class _DasboardState extends State<Dasboard> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = [
-    Text('Dashboard Page'),
-    Text('Input Data Page'),
-    Text('Report STTPP Page'),
-    Text('Perencanaan Page'),
-    Text('Input Kebutuhan Material Page'),
-    Text('Input Dokumen Pendukung Page'),
-    Text('After Sales Page'),
+    Text('Halaman Dasbor'),
+    Text('Halaman Input Data'),
+    Text('Halaman Laporan STTPP'),
+    Text('Halaman Perencanaan'),
+    Text('Halaman Input Kebutuhan Material'),
+    Text('Halaman Input Dokumen Pendukung'),
+    Text('Halaman After Sales'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-        toolbarHeight: 100.0,
-      ),
       body: Stack(
-        children: <Widget>[
+        children: [
           _pages[_selectedIndex],
           if (_selectedIndex == 0) _buildDrawer(),
         ],
@@ -47,23 +43,24 @@ class _dasboardState extends State<dasboard> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 35), // Added SizedBox to adjust position
                 Image.asset(
-                  'assets/images/Logo-REKA.png',
-                  height: 100,
-                  width: 150,
+                  'assets/images/Logo-REKA--300x104 1.png',
+                  height: 130,
+                  width: 250,
                 ),
               ],
             ),
           ),
-          _buildListTile('Dashboard', Icons.dashboard, 0),
+          _buildListTile('Dasbor', Icons.dashboard, 0),
           _buildListTile('Input Data', Icons.input, 1),
-          _buildListTile('Report STTPP', Icons.receipt, 2),
+          _buildSubmenu('Submenu 1', Icons.arrow_forward, 8),
+          _buildSubmenu('Submenu 2', Icons.arrow_forward, 9),
           _buildListTile('Perencanaan', Icons.calendar_today, 3),
           _buildListTile('Input Kebutuhan Material', Icons.assignment, 4),
           _buildListTile('Input Dokumen Pendukung', Icons.file_present, 5),
+          _buildListTile('Laporan STTPP', Icons.receipt, 2),
           _buildListTile('After Sales', Icons.headset_mic, 6),
-          _buildListTile('Logout', Icons.logout, 7),
+          _buildListTile('Keluar', Icons.logout, 7),
         ],
       ),
     );
@@ -74,6 +71,21 @@ class _dasboardState extends State<dasboard> {
       title: Text(title),
       leading: Icon(icon),
       onTap: () {
+        setState(() {
+          _selectedIndex = index;
+        });
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  Widget _buildSubmenu(String title, IconData icon, int index) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(icon),
+      onTap: () {
+        // Tambahkan logika atau perpindahan halaman untuk submenu di sini
+        print('Submenu $title ditekan');
         setState(() {
           _selectedIndex = index;
         });
