90cda82c66040a5cb4236baa12b5e4415cf78cad
cacalawak <risayusnita02@gmail.com>
2024-02-23 13:50:25 +0700

    View Perencanaan

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:RekaChain/perencanaan.dart';
 import 'package:RekaChain/AfterSales/AfterSales.dart';
 import 'package:RekaChain/reportsttpp.dart';
 import 'package:RekaChain/viewikm.dart';
+import 'package:RekaChain/viewperencanaan.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: LoginPage(),
+      home: Vperencanaan(),
     );
   }
 }
---
+++ lib/viewperencanaan.dart
@@ -0,0 +1,368 @@
+import 'package:RekaChain/AfterSales/AfterSales.dart';
+import 'package:RekaChain/dasboard.dart';
+import 'package:RekaChain/inputdokumen.dart';
+import 'package:RekaChain/inputkebutuhan%20material.dart';
+import 'package:RekaChain/login.dart';
+import 'package:RekaChain/perencanaan.dart';
+import 'package:RekaChain/profile.dart';
+import 'package:RekaChain/reportsttpp.dart';
+import 'package:flutter/material.dart';
+
+class Vperencanaan extends StatefulWidget {
+  @override
+  State<Vperencanaan> createState() => _VperencanaanState();
+}
+
+class _VperencanaanState extends State<Vperencanaan> {
+  int _selectedIndex = 0;
+  late double screenWidth;
+  late double screenHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (context) => Vperencanaan(),
+            );
+          default:
+            return null;
+        }
+      },
+      home: Scaffold(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDrawer(),
+            Expanded(
+              child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+                  toolbarHeight: 65,
+                  title: Padding(
+                    padding: EdgeInsets.only(left: screenHeight * 0.02),
+                    child: Text(
+                      'Daftar Data Project',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Donegal One',
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    Padding(
+                      padding: EdgeInsets.only(right: screenHeight * 0.13),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.005,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications_active,
+                              size: 35,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Vperencanaan()),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.account_circle_rounded,
+                              size: 38,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Profile()),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                body: _ListView(),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+      ),
+    );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+      backgroundColor: Color.fromARGB(255, 244, 249, 255),
+      child: Column(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/images/logoreka.png',
+                    height: 130,
+                    width: 250,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _buildListTile('Dashboard', Icons.dashboard, 0, 35),
+          _buildSubMenu(),
+          _buildListTile('After Sales', Icons.headset_mic, 6, 35),
+          _buildListTile('Logout', Icons.logout, 7, 35),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListTile(String title, IconData icon, int index, int size) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+        color: Color.fromARGB(255, 6, 37, 55),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Dashboard(),
+              ),
+            );
+          } else if (index == 6) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AfterSales(),
+              ),
+            );
+          } else {
+            Navigator.pop(context);
+          }
+        }
+      },
+    );
+  }
+
+  Widget _buildSubMenu({IconData? icon}) {
+    return ExpansionTile(
+      title: Row(
+        children: [
+          Icon(
+            icon ?? Icons.input,
+            size: 35,
+            color: Color.fromARGB(255, 6, 37, 55),
+          ),
+          SizedBox(width: 12),
+          Text('Input Data'),
+        ],
+      ),
+      children: [
+        _buildSubListTile('Report STTPP', Icons.receipt, 2, 35),
+        _buildSubListTile('Perencanaan', Icons.calendar_today, 3, 35),
+        _buildSubListTile('Input Kebutuhan Material', Icons.assignment, 4, 35),
+        _buildSubListTile('Input Dokumen Pendukung', Icons.file_present, 5, 35),
+      ],
+    );
+  }
+
+  Widget _buildSubListTile(
+    String title,
+    IconData icon,
+    int index,
+    int size,
+  ) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ReportSTTPP(),
+              ),
+            );
+          } else if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Perencanaan(),
+              ),
+            );
+          } else if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => InputMaterial(),
+              ),
+            );
+          } else if (index == 5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => InputDokumen(),
+              ),
+            );
+          }
+        }
+      },
+    );
+  }
+
+  void _showLogoutDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Logout", style: TextStyle(color: Colors.white)),
+          content: Text("Apakah Anda yakin ingin logout?",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromRGBO(255, 6, 37, 55),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Batal", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: Text("Logout", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+Widget _ListView() {
+  return ListView.separated(
+    itemBuilder: (context, index) {
+      return ListViewItem(index);
+    },
+    separatorBuilder: (context, index) {
+      return Divider(height: 0);
+    },
+    itemCount: 15,
+  );
+}
+
+Widget ListViewItem(int index) {
+  return Container(
+    margin: EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Container(
+            margin: EdgeInsets.only(left: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                message(index),
+              ],
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+Widget message(int index) {
+  double textsize = 14;
+  return Container(
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+          height: 15,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: Text(
+                'R22 - PT. Nugraha Jasa ${index + 1}',
+                maxLines: 3,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 17,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.visibility),
+              onPressed: () {
+                // Add your edit logic here
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                // Add your edit logic here
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // Add your delete logic here
+              },
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
