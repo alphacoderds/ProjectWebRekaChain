7cee0dcd4c0d72f2b5e705076172899e426a2ed3
cacalawak <risayusnita02@gmail.com>
2024-02-26 15:56:54 +0700

    Cetak Perencanaan

---
+++ lib/cetak.dart
@@ -0,0 +1,414 @@
+import 'package:RekaChain/AfterSales/AfterSales.dart';
+import 'package:RekaChain/dasboard.dart';
+import 'package:RekaChain/inputkebutuhanmaterial.dart';
+import 'package:RekaChain/login.dart';
+import 'package:RekaChain/notification.dart';
+import 'package:RekaChain/perencanaan.dart';
+import 'package:RekaChain/profile.dart';
+import 'package:RekaChain/reportsttpp.dart';
+import 'package:RekaChain/viewupload.dart';
+import 'package:flutter/material.dart';
+
+class Cetak extends StatefulWidget {
+  const Cetak({super.key});
+
+  @override
+  State<Cetak> createState() => _CetakState();
+}
+
+class _CetakState extends State<Cetak> {
+  late double screenWidth = MediaQuery.of(context).size.width;
+  late double screenHeight = MediaQuery.of(context).size.height;
+
+  int _selectedIndex = 0;
+  List<String> dropdownItems = [
+    '--Pilih Nama/Kode Project--',
+    'R22-PT. Nugraha Jasa',
+    'PT. INDAH JAYA'
+  ];
+  String? selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        screenWidth = constraints.maxWidth;
+        screenHeight = constraints.maxHeight;
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          onGenerateRoute: (settings) {
+            switch (settings.name) {
+              case '/':
+                return MaterialPageRoute(
+                  builder: (context) => const Cetak(),
+                );
+              default:
+                return null;
+            }
+          },
+          home: Scaffold(
+            body: Padding(
+              padding: EdgeInsets.only(left: screenWidth * 0.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildDrawer(),
+                  Expanded(
+                    child: Scaffold(
+                      appBar: AppBar(
+                        backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+                        toolbarHeight: 65,
+                        actions: [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(right: screenHeight * 0.11),
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width: screenWidth * 0.005,
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.notifications_active,
+                                    size: 33,
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Notifikasi()),
+                                    );
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.account_circle_rounded,
+                                    size: 35,
+                                    color: Color.fromARGB(255, 255, 255, 255),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => Profile()),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      body: Center(
+                        child: Column(
+                          children: [
+                            _buildMainTable(),
+                            ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Back',
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor:
+                                    const Color.fromRGBO(43, 56, 86, 1),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _buildMainTable() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: screenHeight * 0.04),
+          child: Row(
+            children: [
+              Center(
+                child: Container(
+                  width: screenWidth * 0.7,
+                  height: screenHeight * 0.75,
+                  decoration: BoxDecoration(
+                      border: Border.all(),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: screenHeight * 0.09,
+                        horizontal: screenWidth * 0.05),
+                    child: Row(
+                      children: [
+                        SizedBox(width: screenWidth * 0.05),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Container(
+                                      width: 400, // adjust width as needed
+                                      height: 400,
+                                      decoration: BoxDecoration(
+                                          border: Border.all(),
+                                          borderRadius: BorderRadius.circular(
+                                              10)) // adjust height as needed
+
+                                      // color of the box
+                                      ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Print',
+                                    ),
+                                    style: ElevatedButton.styleFrom(
+                                      foregroundColor: Colors.white,
+                                      backgroundColor:
+                                          const Color.fromRGBO(43, 56, 86, 1),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+      backgroundColor: Color.fromARGB(255, 244, 249, 255),
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/images/logoreka.png',
+                    height: 130,
+                    width: 250,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _buildListTile('Dashboard', Icons.dashboard, 0, 35),
+          _buildSubMenu(),
+          _buildListTile('After Sales', Icons.headset_mic, 6, 35),
+          _buildListTile('Logout', Icons.logout, 7, 35),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListTile(String title, IconData icon, int index, int size) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+        color: Color.fromARGB(255, 6, 37, 55),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Dashboard(),
+              ),
+            );
+          } else if (index == 6) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AfterSales(),
+              ),
+            );
+          } else {
+            Navigator.pop(context);
+          }
+        }
+      },
+    );
+  }
+
+  Widget _buildSubMenu({IconData? icon}) {
+    return ExpansionTile(
+      title: Row(
+        children: [
+          Icon(
+            icon ?? Icons.input,
+            size: 35,
+            color: Color.fromARGB(255, 6, 37, 55),
+          ),
+          SizedBox(width: 12),
+          Text('Input Data'),
+        ],
+      ),
+      children: [
+        _buildSubListTile('Report STTPP', Icons.receipt, 2, 35),
+        _buildSubListTile('Perencanaan', Icons.calendar_today, 3, 35),
+        _buildSubListTile('Input Kebutuhan Material', Icons.assignment, 4, 35),
+        _buildSubListTile('Input Dokumen Pendukung', Icons.file_present, 5, 35),
+      ],
+    );
+  }
+
+  Widget _buildSubListTile(
+    String title,
+    IconData icon,
+    int index,
+    int size,
+  ) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ReportSTTPP(),
+              ),
+            );
+          } else if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Perencanaan(),
+              ),
+            );
+          } else if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => InputMaterial(),
+              ),
+            );
+          } else if (index == 5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Cetak(),
+              ),
+            );
+          }
+        }
+      },
+    );
+  }
+
+  void _showFinishDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Simpan Data", style: TextStyle(color: Colors.white)),
+          content: Text("Apakah Anda yakin ingin simpan data?",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Batal", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Cetak()),
+                );
+              },
+              child: Text("Ya", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showLogoutDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Logout", style: TextStyle(color: Colors.white)),
+          content: Text("Apakah Anda yakin ingin logout?",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Batal", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: Text("Logout", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
