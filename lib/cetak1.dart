80595b21fc5d4673bc384776e9a772ac1d71d05b
nabilaeka123 <nabila2004eka@gmail.com>
2024-02-26 16:15:19 +0700

    Membuat halaman cetak barcode

---
+++ lib/cetak1.dart
@@ -0,0 +1,464 @@
+import 'package:RekaChain/AfterSales/AfterSales.dart';
+import 'package:RekaChain/dasboard.dart';
+import 'package:RekaChain/inputdokumen.dart';
+import 'package:RekaChain/inputkebutuhanmaterial.dart';
+import 'package:RekaChain/inputkebutuhanmaterial.dart';
+import 'package:RekaChain/login.dart';
+import 'package:RekaChain/notification.dart';
+import 'package:RekaChain/perencanaan.dart';
+import 'package:RekaChain/profile.dart';
+import 'package:RekaChain/reportsttpp.dart';
+import 'package:RekaChain/viewaftersales.dart';
+import 'package:flutter/material.dart';
+
+class Cetak extends StatefulWidget {
+  @override
+  State<Cetak> createState() => _CetakState();
+}
+
+class _CetakState extends State<Cetak> {
+  int _selectedIndex = 0;
+
+  List<String> dropdownItems = [
+    '--Pilih Nama/Kode Project--',
+    'R22-PT. Nugraha Jasa',
+    'PT. INDAH JAYA'
+  ];
+  String? selectedValue;
+
+  bool isViewVisible = false;
+  late double screenWidth;
+  late double screenHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    screenWidth = MediaQuery.of(context).size.width;
+    screenHeight = MediaQuery.of(context).size.height;
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: (settings) {
+        switch (settings.name) {
+          case '/':
+            return MaterialPageRoute(
+              builder: (context) => Cetak(),
+            );
+          default:
+            return null;
+        }
+      },
+      home: Scaffold(
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildDrawer(),
+            Expanded(
+              child: Scaffold(
+                appBar: AppBar(
+                  backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+                  toolbarHeight: 65,
+                  title: Padding(
+                    padding: EdgeInsets.only(left: screenHeight * 0.02),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 300,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(5),
+                            color: Colors.white,
+                          ),
+                          child: DropdownButton<String>(
+                            alignment: Alignment.center,
+                            hint: Text('--Pilih Nama/Kode Project--'),
+                            value: selectedValue,
+                            borderRadius: BorderRadius.circular(5),
+                            items: dropdownItems.map((String value) {
+                              return DropdownMenuItem<String>(
+                                value: value,
+                                child: Text(value),
+                              );
+                            }).toList(),
+                            onChanged: (newValue) {
+                              setState(() {
+                                selectedValue = newValue;
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  actions: [
+                    Padding(
+                      padding: EdgeInsets.only(right: screenHeight * 0.13),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: screenWidth * 0.005,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.download,
+                              size: 33,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.notifications_active,
+                              size: 35,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Notifikasi(),
+                                ),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.account_circle_rounded,
+                              size: 38,
+                              color: Color.fromARGB(255, 255, 255, 255),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Profile()),
+                              );
+                            },
+                            padding: EdgeInsets.only(
+                                right:
+                                    16.0), // Sesuaikan dengan jarak yang diinginkan
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                body: Stack(
+                  children: [
+                    Container(
+                      child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: screenHeight * 0.01),
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Center(
+                              child: Container(
+                                width: screenWidth * 0.63,
+                                height: screenHeight * 0.70,
+                                padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                                margin: EdgeInsets.all(16.0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black),
+                                  borderRadius: BorderRadius.circular(5.0),
+                                ),
+                                child: _buildMainTable(),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 150,
+                      right: 150,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle your back button action here
+                        },
+                        child: Text('Cetak QR Code'),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle your back button action here
+                        },
+                        child: Text('Back'),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 20,
+                      right: 20,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle your back button action here
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Color.fromARGB(
+                              255, 1, 46, 76), // Background color
+                        ),
+                        child: Text(
+                          'Back',
+                          style: TextStyle(
+                              color: Color.fromARGB(
+                                  255, 226, 228, 231)), // Text color
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainTable() {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.of(context).size.height - 200,
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columnSpacing: 182.0,
+            horizontalMargin: 182.0,
+            columns: [
+              DataColumn(
+                label: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 1.0),
+                  child: Text(
+                    'Nama Produk',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              DataColumn(
+                label: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'No Produk',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              DataColumn(
+                label: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        'QR Code',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+            rows: [
+              DataRow(cells: [
+                DataCell(Text('')),
+                DataCell(Text('')),
+                DataCell(Text('')),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+            ),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Image.asset(
+                    'assets/images/logoreka.png',
+                    height: 130,
+                    width: 250,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _buildListTile('Dashboard', Icons.dashboard, 0, 35),
+          _buildSubMenu(),
+          _buildListTile('After Sales', Icons.headset_mic, 6, 35),
+          _buildListTile('Logout', Icons.logout, 7, 35),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListTile(String title, IconData icon, int index, int size) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+        color: Color.fromARGB(255, 6, 37, 55),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 0) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Dashboard(),
+              ),
+            );
+          } else if (index == 6) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => AfterSales(),
+              ),
+            );
+          } else {
+            Navigator.pop(context);
+          }
+        }
+      },
+    );
+  }
+
+  Widget _buildSubMenu({IconData? icon}) {
+    return ExpansionTile(
+      title: Row(
+        children: [
+          Icon(
+            icon ?? Icons.input,
+            size: 35,
+            color: Color.fromARGB(255, 6, 37, 55),
+          ),
+          SizedBox(width: 12),
+          Text('Input Data'),
+        ],
+      ),
+      children: [
+        _buildSubListTile('Report STTPP', Icons.receipt, 2, 35),
+        _buildSubListTile('Perencanaan', Icons.calendar_today, 3, 35),
+        _buildSubListTile('Input Kebutuhan Material', Icons.assignment, 4, 35),
+        _buildSubListTile('Input Dokumen Pendukung', Icons.file_present, 5, 35),
+      ],
+    );
+  }
+
+  Widget _buildSubListTile(
+    String title,
+    IconData icon,
+    int index,
+    int size,
+  ) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(
+        icon,
+        size: size.toDouble(),
+        color: Color.fromARGB(255, 6, 37, 55),
+      ),
+      onTap: () {
+        if (index == 7) {
+          _showLogoutDialog();
+        } else {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 2) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ReportSTTPP(),
+              ),
+            );
+          } else if (index == 3) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Perencanaan(),
+              ),
+            );
+          } else if (index == 4) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => InputMaterial(),
+              ),
+            );
+          } else if (index == 5) {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => InputDokumen(),
+              ),
+            );
+          }
+        }
+      },
+    );
+  }
+
+  void _showLogoutDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Logout", style: TextStyle(color: Colors.white)),
+          content: Text("Apakah Anda yakin ingin logout?",
+              style: TextStyle(color: Colors.white)),
+          backgroundColor: const Color.fromRGBO(43, 56, 86, 1),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Batal", style: TextStyle(color: Colors.white)),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                );
+              },
+              child: Text("Logout", style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+void main() {
+  runApp(Cetak());
+}
